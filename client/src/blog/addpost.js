589232fc392681b30ce433801075d32cb90c5e60
ANTONETTI <elzzr@c02z74h4lvdq.na.ds.monsanto.com>
2020-01-31 11:30:23 -0600

    Initial commit

---
+++ client/src/blog/addpost.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+import history from '../utils/history';
+import TextField from '@material-ui/core/TextField';
+import { connect } from 'react-redux'
+
+class AddPost extends Component {
+
+    handleSubmit = (event) => {
+        event.preventDefault()
+        const user_id = this.props.db_profile[0].uid
+        const username = this.props.db_profile[0].username
+        const data = {title: event.target.title.value,
+            body: event.target.body.value,
+            username: username,
+            uid: user_id}
+        axios.post('/api/post/posttodb', data)
+            .then(response => console.log(response))
+            .catch((err) => console.log(err))
+            .then(setTimeout(() => history.replace('/'), 700))
+    }
+
+    render() {
+        return(
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <TextField
+                        id='title'
+                        label='Title'
+                        margin='normal'
+                    />
+                    <br />
+                    <TextField
+                        id='body'
+                        label='Body'
+                        multiline
+                        rows='4'
+                        margin="normal"
+                    />
+                    <br />
+                    <button type='submit'> Submit </button>
+                </form>
+                <br />
+                <button onClick={() => history.replace('/posts')}> Cancel </button> 
+            </div>
+
+        )
+    }
+}
+
+export default connect(mapStateToProps)(AddPost)
